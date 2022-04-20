4d713ba3e2f4410aa1baa92a55e8a52e9aadfa46
routus <rbfla8426@gmail.com>
2022-04-20 23:51:35 +0900

    no message

--- src/main/java/com/example/demo/controller/Marker_userController.java
+++ src/main/java/com/example/demo/controller/Marker_userController.java
@@ -2,9 +2,11 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -23,7 +25,14 @@ public class Marker_userController {
 		this.mapper = mapper;
 	}
 	
-	@GetMapping(value = "/markers/users")
+	@MessageMapping(value = "/markers/users")
+	@SendTo(value = "/topic/markers/users")
+	public String getMarker_userList(String marker) {
+		return marker;
+		//return mapper.getMarker_userList();
+	}
+			
+/*	@GetMapping(value = "/markers/users")
 	public List<Marker_user> getMarker_userList() {
 		return mapper.getMarker_userList();
 	}
@@ -47,4 +56,5 @@ public class Marker_userController {
 	public void putUser(@PathVariable("id") String id, @RequestBody Marker_user marker_userUpdate) {
 	mapper.updateMarker_user(marker_userUpdate);
 	}
+	*/
 }
